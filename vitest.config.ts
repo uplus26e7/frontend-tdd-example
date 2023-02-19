87ffc4e395633849ce729022fb0ed8477f975af4
Yuya Shintani <yuya.shintani.uplus26e7@gmail.com>
2023-02-19 15:12:50 +0900

    🔨 Update test tools

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./tests/setup.ts",
-	},
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+    coverage: {
+      reporter: ["html"],
+    },
+  },
 });
